f9ef1f1558e977cfa5bbd248b882f4740f2d9e54
GenerelSchwerz <xXSG.KingXx@gmail.com>
2024-07-26 18:05:48 -0400

    good?

--- pokerbot/triplejack/TJClient.py
+++ pokerbot/triplejack/TJClient.py
@@ -7,23 +7,31 @@ from .base import TJPokerDetect, TJInteract, TJEventEmitter
 
 def run_ticks(_bot: AClient, time_split=2):
     import cv2
+
     last_time = time.time()
     while True:
         ss = _bot.interactor._ss()
 
         cv2.imwrite(f"./midrun/{int(time.time() * 100_000) / 100_000}.png", ss)
 
-
         _bot.event_handler.tick(ss)
         now = time.time()
         dur = max(0, time_split - (now - last_time))
-        print("Sleeping for", round(dur * 1000) / 1000, "seconds. Been", round((now - last_time) * 1000) / 1000, "seconds taken to calculate since last tick.",round(( dur + now - last_time) * 1000) / 1000, "seconds total")
+        print(
+            "Sleeping for",
+            round(dur * 1000) / 1000,
+            "seconds. Been",
+            round((now - last_time) * 1000) / 1000,
+            "seconds taken to calculate since last tick.",
+            round((dur + now - last_time) * 1000) / 1000,
+            "seconds total",
+        )
         time.sleep(dur)
         last_time = time.time()
 
 
-def launch_user(username: str, password: str):
-    from ..all.algoLogic import AlgoDecisions   
+def launch_user(username: str, password: str, time_split=2):
+    from ..all.algoLogic import AlgoDecisions
 
     detector = TJPokerDetect()
     detector.load_images()
@@ -38,12 +46,12 @@ def launch_user(username: str, password: str):
         detector=detector,
         interactor=interactor,
         logic=logic,
-        debug=True
+        debug=True,
     )
 
     try:
         bot.start(username, password)
-        run_ticks(bot, )
+        run_ticks(bot, time_split)
     except KeyboardInterrupt:
         print("bot finished")
         bot.stop()
@@ -53,8 +61,10 @@ def launch_user(username: str, password: str):
         traceback.print_exc()
         bot.stop()
 
+
 def main():
     import os
+
     # clear midrun
 
     os.makedirs("./midrun", exist_ok=True)
@@ -62,36 +72,29 @@ def main():
     for file in os.listdir("./midrun"):
         os.remove(f"./midrun/{file}")
 
-
     from concurrent.futures import ThreadPoolExecutor
+
     try:
         accounts = []
 
-        with open('pokerbot/config.json') as config_file:
-            config = json.load(config_file)["triplejack"]
-            accounts = config["accounts"]
+        with open("pokerbot/config.json") as config_file:
+            tj_cfg = json.load(config_file)["triplejack"]
+            accounts = tj_cfg["accounts"]
 
-
-        
         with ThreadPoolExecutor() as executor:
 
             for info in accounts:
                 username = info["username"]
                 password = info["password"]
-                executor.submit(launch_user, username, password)
+                time_split = tj_cfg["secondsPerTick"]
+                executor.submit(launch_user, username, password, time_split)
 
-       
         # wait until all threads are done
         executor.shutdown(wait=True)
-
-
-
 
     except FileNotFoundError:
         print("Config file not found")
         return
-
-    
 
 
 if __name__ == "__main__":
