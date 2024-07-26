c314d23a4f46e9698b16826c5b5611c58be7f154
GenerelSchwerz <xXSG.KingXx@gmail.com>
2024-07-26 18:56:11 -0400

    good?

--- pokerbot/triplejack/driver_creator.py
+++ pokerbot/triplejack/driver_creator.py
@@ -1,31 +1,42 @@
 from selenium import webdriver
 
+
+import pywinctl
+
+
 def create_tj_driver(headless=False):
 
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
 
-    options.add_argument("--no-sandbox")
-    options.add_argument("--use-angle=vulkan")
-    options.add_argument("--enable-features=Vulkan")
-    options.add_argument("--disable-vulkan-surface")
-    options.add_argument("--enable-unsafe-webgpu")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--use-angle=vulkan")
+    # options.add_argument("--enable-features=Vulkan")
+    # options.add_argument("--disable-vulkan-surface")
+    # options.add_argument("--enable-unsafe-webgpu")
 
-    options.add_argument("--mute-audio")
+    # options.add_argument("--mute-audio")
 
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_argument("--disable-infobars")
+    # options.add_experimental_option("useAutomationExtension", False)
+    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # options.add_argument("--disable-infobars")
+    # options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
+    # use /usr/bin/google-chrome-stable
+    # options.binary_location = "/usr/bin/google-chrome-stable"
+    
     if headless:
-        options.add_argument("--headless=new")
-        options.add_argument("--window-size=1920,1080")
-        driver = webdriver.Chrome(options=options)
+        options.add_argument("--headless")
+        # options.add_argument("--window-size=1280,720")
+        driver = webdriver.Firefox(options=options)
         # driver.maximize_window() # why is this setting to 4k lol, no need
     else:
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--start-maximized")
-        driver = webdriver.Chrome(options=options)
+        options.add_argument("--window-size=1280,720")
+        # options.add_argument("--start-maximized")
+        driver = webdriver.Firefox(options=options)
 
-    driver.set_window_size(1920, 1080)
+
+    driver.set_window_position(0,0)
+    driver.set_window_size(1280, 720, windowHandle="current")
+   
 
     return driver
